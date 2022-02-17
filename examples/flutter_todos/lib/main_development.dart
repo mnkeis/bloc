dec862ba04d7dab0c89f9f3199a26cd5283a5adf
Mauro Casse <mnkeis@gmail.com>
2022-02-17 00:29:10 -0300

    Finished firebase configuration

--- examples/flutter_todos/lib/main_development.dart
+++ examples/flutter_todos/lib/main_development.dart
@@ -1,13 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firestore_todos_api/firestore_todos_api.dart';
 import 'package:flutter_services_binding/flutter_services_binding.dart';
 import 'package:flutter_todos/bootstrap.dart';
-import 'package:local_storage_todos_api/local_storage_todos_api.dart';
+// import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 
 Future<void> main() async {
   FlutterServicesBinding.ensureInitialized();
 
-  final todosApi = LocalStorageTodosApi(
-    plugin: await SharedPreferences.getInstance(),
-  );
+  // final todosApi = LocalStorageTodosApi(
+  //   plugin: await SharedPreferences.getInstance(),
+  // );
+
+  final todosApi = FirestoreTodosApi(firestore: FirebaseFirestore.instance);
 
   bootstrap(todosApi: todosApi);
 }

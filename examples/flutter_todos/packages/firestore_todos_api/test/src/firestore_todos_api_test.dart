af3abd668f6943d7fda6fb8eebf5378a7fef2a33
Mauro Casse <mnkeis@gmail.com>
2022-02-16 23:18:58 -0300

    Added test to FirestoreTodosApi instantiation

--- examples/flutter_todos/packages/firestore_todos_api/test/src/firestore_todos_api_test.dart
+++ examples/flutter_todos/packages/firestore_todos_api/test/src/firestore_todos_api_test.dart
@@ -1,11 +1,27 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firestore_todos_api/firestore_todos_api.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
 
 void main() {
+  final firestoreInstance = MockFirebaseFirestore();
+
+  setUp(() {
+    registerFallbackValue(firestoreInstance);
+  });
+
   group('FirestoreTodosApi', () {
     test('can be instantiated', () {
-      expect(FirestoreTodosApi(), isNotNull);
+      expect(
+        FirestoreTodosApi(
+          firestore: firestoreInstance,
+          userId: '',
+        ),
+        isNotNull,
+      );
     });
   });
 }
